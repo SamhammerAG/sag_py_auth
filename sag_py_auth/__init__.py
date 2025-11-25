665c1736c8d994609ea94e23d199dd8642e240d8
oschulz <oschulz@samhammer.de>
2025-11-25 22:33:40 +0100

    KIT-4555 introduce ruff

--- sag_py_auth/__init__.py
+++ sag_py_auth/__init__.py
@@ -1,5 +1,4 @@
-# pyright: reportUnusedImport=none
-from .auth_context import get_token
-from .jwt_auth import JwtAuth
-from .models import AuthConfig, Token, TokenRole, UserInfoLogRecord
-from .user_name_logging_filter import UserNameLoggingFilter
+from .auth_context import get_token # noqa: F401
+from .jwt_auth import JwtAuth # noqa: F401
+from .models import AuthConfig, Token, TokenRole, UserInfoLogRecord # noqa: F401
+from .user_name_logging_filter import UserNameLoggingFilter # noqa: F401
