78e34a308da4d7e0100770015806f3758fcf4f58
Alexander "Vijo" Ott <ottalexanderdev@gmail.com>
2026-06-26 13:34:07 +0200

    revert last commit

--- sag_py_auth/__init__.py
+++ sag_py_auth/__init__.py
@@ -1,6 +1,4 @@
 from .auth_context import get_token  # noqa: F401
 from .jwt_auth import JwtAuth  # noqa: F401
-from .models import TokenRole  # noqa: F401
-from .models import AuthConfig, Token, UserInfoLogRecord
-from .token_requester import get_client_credentials_token  # noqa: F401
+from .models import AuthConfig, Token, TokenRole, UserInfoLogRecord  # noqa: F401
 from .user_name_logging_filter import UserNameLoggingFilter  # noqa: F401
